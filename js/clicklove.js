bacbd7849121ce62a9b6cb26ff45f131afd157c9
DantePeach <56532734+DantePeach@users.noreply.github.com>
2019-10-22 18:36:51 +0800

    Site updated: 2019-10-22 18:36:49

--- js/clicklove.js
+++ js/clicklove.js
@@ -1,7 +1,7 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
  $("body").click(function(e) {
- var a = new Array("欧拉");
+ var a = new Array("小鸟游六花", "斯卡蒂", "千反田爱瑠", "星尘", "四宫辉夜", "椎名真白", "加藤惠" ,"她们", "都是", "我老婆", "不接受反驳", "╭(╯^╰)╮");
  var $i = $("<span/>").text(a[a_idx]);
  a_idx = (a_idx + 1) % a.length;
  var x = e.pageX,
